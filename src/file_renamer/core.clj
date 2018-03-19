4c7d5c13cd2e4506041bd02b3b2b4b408381f328
John Lynch <wizardfix@gmail.com>
2018-03-19 18:27:34 +0000

    added confirmation dialogue

--- src/file_renamer/core.clj
+++ src/file_renamer/core.clj
@@ -25,11 +25,11 @@
     "Map a fn to rename a file (by replacing all substrings of whitespace in the name with the given separator) 
     over all files and dirs in the given directory"
     [direc separator]
-    (pp/pprint "Getting list of directories...")
+    (println "Getting list of directories...")
 
     ; need to rename directories deepest first to obviate problems with changed references, so sort them;
     (let [dir-list 	  (filter #(.isDirectory (as-file %)) (reverse (sort-by  #(-> % as-file .toPath .getNameCount) (vec (traverse-dir direc)))))]
-	  (pp/pprint "Renaming directories...")
+	  (println "Renaming directories...")
       (doseq [dir dir-list] 
   		(let [d  (as-file dir)
 	  		  dname (.getName d)]
@@ -38,10 +38,12 @@
     ; now get an updated file list, then rename files; order unimportant
     (let [file-list       (filter #(.isFile (as-file %)) (vec (traverse-dir direc)))
   	      new-file-list   (vec (map #(replace-spaces % separator) file-list))]
-  	  (pp/pprint "Renaming files...")
+  	  (println "Renaming files...")
       (doseq [[f1 f2] (map list file-list new-file-list)] (when-not (= f1 f2) (.renameTo (as-file f1) (as-file f2))))))
 
 (defn -main
-    [direc separator]
-	(despace-filenames direc separator)
-	(pp/pprint "Done!"))
+    [^String direc ^String separator]
+    (println "Do you really want to replace all blocks of whitespace in all directories and files in ")
+    (println  (str direc " with the string '" separator "'? - Y/n")) 
+    (if (= (read-line) "Y") (despace-filenames direc separator) (System/exit 0))
+	(println "Done!"))
